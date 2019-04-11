5a09f6e68dc99d96e5596602d8483f275a718b6a
Francesco Monti <f.monti@webcrafters.it>
2019-04-11 11:13:17 +0200

    fix view create

--- src/MoonServiceProvider.php
+++ src/MoonServiceProvider.php
@@ -26,7 +26,7 @@ class MoonServiceProvider extends ServiceProvider
     {
         include __DIR__.'/routes.php';
         $this->loadMigrationsFrom(__DIR__.'/Migrations');
-        $this->publishes([__DIR__.'/Controllers/MoonController.php' => base_path('app/Http/Controllers')], 'Moon');
+        $this->publishes([__DIR__.'/Controllers' => base_path('app/Http/Controllers')], 'Moon');
         $this->publishes([__DIR__.'/resources/views' => base_path('resources/views/vendor/Moon')], 'Moon');
     }
 }
--- src/resources/views/Module/create.blade.php
+++ src/resources/views/Module/create.blade.php
@@ -1,7 +1,7 @@
 @extends('vendor.Moon.layouts.app')
 
 @section('content-title')
-<b>{{__($title)}}</b> {{__($action)}}
+<b>{{__($title)}}</b>: {{__($action)}}
 @endsection
 
 @section('content-description')
@@ -13,8 +13,8 @@
     <div class="col-md-8 col-md-offset-2">
         <div class="ibox float-e-margins">
             <div class="ibox-content">
-                <?= Former::open(action($controllerName.'@store')); ?>
-                    <?php App\MoonForm::printFields($fields); ?>
+                <?= Former::open($formUrl);?>
+                    <?php Jxd\Moon\MoonForm::printFields($fields); ?>
                     <?= Former::actions()->large_primary_submit('Submit')->large_inverse_reset('Reset'); ?>
                 <?= Former::close() ?>
             </div>
